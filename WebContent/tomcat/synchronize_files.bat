cf87da11bb9f33338eae7be7bab4145c6b7eb781
zzx2856 <zzx2856@gmail.com>
2014-07-12 21:21:41 +0800

    更新tomcat目录下的Root下的数据库连接

--- WebContent/tomcat/synchronize_files.bat
+++ WebContent/tomcat/synchronize_files.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 XCOPY %cd%\common F:\Work\Liferay\liferay-portal-tomcat-6.0.6\tomcat-6.0.29\webapps\ROOT\html\common /D /S /C /H /R /Y
 XCOPY %cd%\company F:\Work\Liferay\liferay-portal-tomcat-6.0.6\tomcat-6.0.29\webapps\ROOT\html\company /D /S /C /H /R /Y
+XCOPY %cd%\classes F:\Work\Liferay\liferay-portal-tomcat-6.0.6\tomcat-6.0.29\webapps\ROOT\WEB-INF\classes /D /S /C /H /R /Y
 PAUSE
